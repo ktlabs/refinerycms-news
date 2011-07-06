00bfb9462e37cc64c0fdde0bfb9f20d02d2d96f2
mbashirov <ya.eskaflone@yandex.ru>
2011-07-06 11:49:47 +0400

    Fix to make it work when creating new installation.

--- db/migrate/3_translate_news_items.rb
+++ db/migrate/3_translate_news_items.rb
@@ -5,8 +5,7 @@ class TranslateNewsItems < ActiveRecord::Migration
     unless defined?(::NewsItem::Translation) && ::NewsItem::Translation.table_exists?
       ::NewsItem.create_translation_table!({
         :title => :string,
-        :body => :text,
-        :external_url => :string
+        :body => :text
       }, {
         :migrate_data => true
       })
