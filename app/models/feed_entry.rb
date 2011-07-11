797da7b1fa13fffc40042e4e188bed2f30ae4a4e
mbashirov <ya.eskaflone@yandex.ru>
2011-07-11 15:54:40 +0400

    Fixed error when entries are empty

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -51,67 +51,69 @@ class FeedEntry < ActiveRecord::Base
   private
 
   def self.add_entries(entries, feed_source, start_date)
-    entries.reverse.each do |entry|
-      if !exists?(:entry_id => entry.id) &&
-        (start_date.nil? || (entry.published >= start_date))
-
-        entry.content.nil? ?
-          (entry.summary.nil? ?
-            unparsed_content = entry.title :
-            upparsed_content = entry.summary) :
-          unparsed_content = entry.content
-
-        feed_source.pattern.empty? ?
-          parsed_content = unparsed_content :
-          parsed_content = Nokogiri::HTML(unparsed_content).at_css(feed_source.pattern).inner_html
-
-        parsed_img     = Nokogiri::HTML(unparsed_content).at_css(feed_source.img_pattern)
-
-        if parsed_img.nil?
-          img_id = nil
-        else
-          image_url      = parsed_img.nil? ? nil :  parsed_img.attributes["src"].value
-
-          img_plain      = HTTParty.get(image_url)
-          img_ext        = img_plain.headers["content-type"].sub("image/", "")
-
-          img_file = Tempfile.new("news_items_image.#{img_ext}")
-          img_file.binmode
-          img_file.write(img_plain.body)
-          img_file.rewind
-
-          img = Image.create(
-            :image => img_file
+    unless entries.blank?
+      entries.reverse.each do |entry| 
+        if !exists?(:entry_id => entry.id) && 
+          (start_date.nil? || (entry.published >= start_date))
+    
+          entry.content.nil? ?
+            (entry.summary.nil? ?
+              unparsed_content = entry.title :
+              upparsed_content = entry.summary) :
+            unparsed_content = entry.content
+    
+          feed_source.pattern.empty? ?
+            parsed_content = unparsed_content :
+            parsed_content = Nokogiri::HTML(unparsed_content).at_css(feed_source.pattern).inner_html
+    
+          parsed_img     = Nokogiri::HTML(unparsed_content).at_css(feed_source.img_pattern)
+    
+          if parsed_img.nil?
+            img_id = nil
+          else
+            image_url      = parsed_img.nil? ? nil :  parsed_img.attributes["src"].value
+    
+            img_plain      = HTTParty.get(image_url)
+            img_ext        = img_plain.headers["content-type"].sub("image/", "")
+    
+            img_file = Tempfile.new("news_items_image.#{img_ext}")
+            img_file.binmode
+            img_file.write(img_plain.body)
+            img_file.rewind
+    
+            img = Image.create(
+              :image => img_file
+            )
+            img.image_name = "news_item_image.#{img_ext}"
+            img.image_ext  = img_ext
+            img.save!
+            img_id = img.id
+    
+            img_file.close!
+          end
+    
+          create!(
+            :name           => entry.title,
+            :summary        => entry.summary,
+            :url            => entry.url,
+            :published_at   => entry.published,
+            :entry_id       => entry.id,
+            :feed_source_id => feed_source.id,
+            :content        => parsed_content
           )
-          img.image_name = "news_item_image.#{img_ext}"
-          img.image_ext  = img_ext
-          img.save!
-          img_id = img.id
-
-          img_file.close!
+          NewsItem.create(
+            :title => entry.title,
+            :body => parsed_content,
+            :publish_date => DateTime.now,
+            :created_at => entry.published,
+            :feed_source_id => feed_source.id,
+            :image_id => img_id
+          )
         end
-
-        create!(
-          :name           => entry.title,
-          :summary        => entry.summary,
-          :url            => entry.url,
-          :published_at   => entry.published,
-          :entry_id       => entry.id,
-          :feed_source_id => feed_source.id,
-          :content        => parsed_content
-        )
-        NewsItem.create(
-          :title => entry.title,
-          :body => parsed_content,
-          :publish_date => DateTime.now,
-          :created_at => entry.published,
-          :feed_source_id => feed_source.id,
-          :image_id => img_id
-        )
       end
+    
+      feed_source.last_entry_url = entries.first.url
+      feed_source.save
     end
-
-    feed_source.last_entry_url = entries.first.url
-    feed_source.save
   end
 end
